77f07347026bf7861ad24cf7ead6dc4b526e71fc
ax95877 <ax95877@gmail.com>
2025-10-06 12:31:53 -0600

    Initial Commit

---
+++ Contacts/ContactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsApp.swift
+//  Contacts
+//
+//  Created by Alex on 2025-10-06.
+//
+
+import SwiftUI
+
+@main
+struct ContactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Contacts/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Contacts
+//
+//  Created by Alex on 2025-10-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
